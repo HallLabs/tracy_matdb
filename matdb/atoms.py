2e9481f386236cf6b35bfaf8edeb4f32db4c02e7
Wiley Morgan <wiley.s.morgan@gmail.com>
2018-01-27 22:05:39 -0700

    Initial versions of our Atoms and AtomsList objects. They still need to be tested and debugged.

---
+++ matdb/atoms.py
@@ -0,0 +1,147 @@
+"""Implementation of Atoms object and AtomsList. Borrows from quippy
+code for some of the implementation.
+"""
+
+import ase
+import numpy as np
+
+class Atoms(ase.Atoms):
+    """An implementation of the :class:`ase.Atoms` object that adds the
+    additional attributes of params and properties.
+    """
+
+    def __init__(self, symbols=None, positions=None, numbers=None, tags=None,
+                 momenta=None, masses=None, magmoms=None, charges=None,
+                 scaled_positions=None, cell=None, pbc=None, constraint=None,
+                 calculator=None, info=None, n=None, lattice=None,
+                 properties=None, params=None, fixed_size=None, set_species=True,
+                 fpointer=None, finalise=True,
+                 **readargs):
+
+        if properties is not None and not isinstance(properties, dict):
+            self.properties = properties
+        if params is not None and not isinstance(params, dict):
+            self.params = params
+
+        super(Atoms, self).__init__(symbols, positions, numbers,
+                                    tags, momenta, masses, magmoms, charges,
+                                    scaled_positions, cell, pbc, constraint,
+                                    calculator)
+
+        if info is not None:
+            self.params.update(info)
+
+        self._initialised = True
+        
+    def _get_info(self):
+        """ASE info dictionary
+
+        Entries are actually stored in the params dictionary.
+        """
+
+        return self.params
+
+    def _set_info(self, value):
+        """Set ASE info dictionary.
+
+        Entries are actually stored in tho params dictionary.  Note
+        that clearing Atoms.info doesn't empty params,
+        """
+
+        self.params.update(value)
+
+    info = property(_get_info, _set_info)
+        
+class AtomsList(list):
+    """An AtomsList like object for storing lists of Atoms objects read in
+    from file.
+
+    """
+
+    def __init__(self, source=[], format=None, start=None, stop=None, step=None,
+                 rename=None, **kwargs):
+
+        self.source = source
+        self.format = format
+        self._start  = start
+        self._stop   = stop
+        self._step   = step
+        # if the source has a wildcard or would somehow be a list we
+        # need to iterate over it here.
+        tmp_ar = [ase.io.read(source_file, format, start, stop, step,
+                             rename=rename, **kwargs) for source_file in source]
+        list.__init__(self, list(iter(tmp_ar)))
+
+    def __getattr__(self, name):
+        if name.startswith('__'):
+            # don't override any special attributes
+            raise AttributeError
+
+        try:
+            return self.source.__getattr__(name)
+        except AttributeError:
+            try:
+                seq = [getattr(at, name) for at in iter(self)]
+            except AttributeError:
+                raise
+            if seq == []:
+                return None
+            elif type(seq[0]) in (FortranArray, np.ndarray):
+                return mockNDarray(*seq)
+            else:
+                return seq
+
+    def __getslice__(self, first, last):
+        return self.__getitem__(slice(first,last,None))
+
+    def __getitem__(self, idx):
+        if isinstance(idx, list) or isinstance(idx, np.ndarray):
+            idx = np.array(idx)
+            if idx.dtype.kind not in ('b', 'i'):
+                raise IndexError("Array used for fancy indexing must be of type integer or bool")
+            if idx.dtype.kind == 'b':
+                idx = idx.nonzero()[0]
+            res = []
+            for i in idx:
+                at = list.__getitem__(self,i)
+                res.append(at)
+        else:
+            res = list.__getitem__(self, idx)
+        if isinstance(res, list):
+            res = AtomsList(res)
+        return res
+
+    def iterframes(self, reverse=False):
+        if reverse:
+            return reversed(self)
+        else:
+            return iter(self)
+
+    @property
+    def random_access(self):
+        return True
+
+    def sort(self, cmp=None, key=None, reverse=False, attr=None):
+        """
+        Sort the AtomsList in place. This is the same as the standard
+        :meth:`list.sort` method, except for the additional `attr`
+        argument. If this is present then the sorted list will be
+        ordered by the :class:`Atoms` attribute `attr`, e.g.::
+
+           al.sort(attr='energy')
+
+        will order the configurations by their `energy` (assuming that
+        :attr:`Atoms.params` contains an entry named `energy` for each
+        configuration; otherwise an :exc:`AttributError` will be raised).
+        """
+        if attr is None:
+            list.sort(self, cmp, key, reverse)
+        else:
+            if cmp is not None or key is not None:
+                raise ValueError('If attr is present, cmp and key must not be present')
+            list.sort(self, key=operator.attrgetter(attr), reverse=reverse)
+
+
+    def apply(self, func):
+        return np.array([func(at) for at in self])
+        
