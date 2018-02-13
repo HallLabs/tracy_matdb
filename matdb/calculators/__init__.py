56aa71df3fd8f5fde052b399031a9bafd9c1a1e1
Conrad Rosenbrock <rosenbrockc@gmail.com>
2018-02-13 12:27:21 -0700

    Refactor get for calculator modules to reduce code duplication.

--- matdb/calculators/__init__.py
+++ matdb/calculators/__init__.py
@@ -1,3 +1,23 @@
 from .vasp import AsyncVasp as Vasp
 from .quip import SyncQuip as Quip
 from .aflux import AsyncAflow as Aflow
+
+def get_calculator_module(calcargs):
+    """Returns the module corresponding to the calculator mentioned in
+    `calcargs`.
+
+    Args:
+        calcargs (dict): the "calculator" dictionary that is part of the
+          arguments for a db group.
+    """
+    from matdb import calculators
+    from inspect import getmodule
+    cls = getattr(calculators, calcargs["name"])
+    mod = None
+    
+    try:
+        mod = getmodule(cls)
+    except:
+        pass
+
+    return mod
