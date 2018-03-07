d76005f0959cad1fca6021c63cffffca3805246d
Conrad Rosenbrock <rosenbrockc@gmail.com>
2018-03-07 10:55:12 -0800

    Major refactor of phonon plotter; works off hessian as base quantity.

--- matdb/phonons.py
+++ matdb/phonons.py
@@ -3,6 +3,7 @@
 import numpy as np
 from os import path
 from matdb.base import testmode
+from matdb.utility import chdir
 
 def _ordered_unique(items):
     """Returns the list of unique items in the list while still *preserving* the
@@ -10,158 +11,159 @@ def _ordered_unique(items):
     """
     seen = set()
     seen_add = seen.add
-    return [x for x in seq if not (x in seen or seen_add(x))]
+    return [x for x in items if not (x in seen or seen_add(x))]
 
-def _get_calculator(name, potfile):
-    """Returns an ASE-compatible calculator to use for the phonon calculations.
-
+def roll(hessian):
+    """Rolls the specified hessian into the `phonopy` format.
+    
     Args:
-        name (str): name of the potential type; valid options are ["GAP",
-          "MTP"].
-        potfile (str): full path to the potential file that the potential should
-          use. For GAP, this is a `.xml` file; for MTP, it is a `.ini`.
+        hessian (numpy.ndarray): of shape `n_atoms * 3`.
     """
-    from os import getcwd, chdir
-    from quippy.potential import Potential
+    n = hessian.shape[0]/3
+    result = np.zeros((n, n, 3, 3), dtype='double')
     
-    #GAP doesn't like relative paths when their are multiple binary files
-    #representing the various inter-atomic potentials. We want to switch to the
-    #phonon directory so that the phonon files are cached in the right place;
-    #but first we need to change into the potential directory.
-    curdir = getcwd()
-    potdir = path.dirname(potfile)
-    chdir(potdir)
+    for i in range(n):
+        for j in range(n):
+            result[i, j] = hessian[i*3:(i+1)*3, j*3:(j+1)*3]
 
-    try:
-        if name.lower() == "mtp":
-            from lammpslib import LAMMPSlib
-            otypes = _ordered_unique(AgPd.get_chemical_symbols())
-            atom_types = {s: i+1 for i, s in enumerate(otypes)}
-            header = ["units metal",
-                      "dimension 3",
-                      "boundary p p p",
-                      "atom_style atomic",
-                      "atom_modify map array"]
-            cmds = ["pair_style MLIP {}".format(potfile),
-                    "pair_coeff * *",
-                    "mass 1 106.42", #Pd
-                    "mass 2 107.87", #Ag
-                    "neighbor 2.0 bin",
-                    "neigh_modify delay 10 check yes"]
-            pot = LAMMPSlib(lmpcmds=cmds, atom_types=atom_types,
-                            lammps_header=header, keep_alive=True)
-        else:
-            pot = Potential("IP GAP", param_filename=potfile)
-    finally:
-        #If there is an initialization error in either GAP or MTP, we don't want
-        #the current directory to be different than it was before we came into
-        #this method.
-        chdir(curdir)
+    return result
 
-    return pot
+def _calc_bands(atoms, hessian, supercell=(1, 1, 1), outfile=None, grid=None):
+    """Calculates the band structure for the given Hessian matrix.
 
-def calc(atoms, fit, kpath, cachedir, supercell=4, delta=0.01, Npts=100,
-         potname="GAP"):
-    """Calculates the phonon band structure for the specified QUIP IP parameter
-    file.
-
-    .. warning:: If the delta given to this routine does *not* match the delta
-      used in the phonon calculations (`phonopy` uses the
-      `DISPLACEMENT_DISTANCE` tag to set this value). Since the `phonopy`
-      default is 0.01 A, we use that same value here.
-    
     Args:
-        atoms (quippy.Atoms): atomic structure information.
-        fit (matdb.fitting.basic.Trainer): potential to run the calculation for.
-        kpath (list): of :class:`numpy.ndarray` specifying the points in k-space
-          that should be visited.
-        cachedir (str): path to the directory where phonon calculations are
-          cached. The cache is sorted by system (using chemical formula), *and*
-          by the name of the potential being used.
-        supercell (int or list): number of times to duplicate the cell when
-          forming the supercell; if an `int` is given, the cell is duplicated
-          equally in all three dimensions.
-        delta (float): displacement in Angstroms of each atom when computing the
-          phonons. 
-        Npts (int): number of points to sample across the k-path.
-        potname (str): one of ["MTP", "GAP"] specifying whether to use the
-          `lammpslib` and MTP :class:`ase.Calculator` to interface with LAMMPS
-          instead of a `quippy` GAP potential.
+        atoms (matdb.Atoms): atoms object corresponding to the *primitive*
+          cell. The specified supercell matrix should  result in a number
+          of atoms that matches the dimensionality of the Hessian.
+        supercell (tuple): of `int` supercell matrix components; can have either
+          3 or 9 components.
+        hessian (numpy.ndarray): with shape `(natoms*3, natoms*3)`.
+        grid (list): of `int` specifying the number of divisions in k-space
+          along each reciprocal unit vector.
+        outfile (str): path to the output `band.yaml` file that should be
+          created by this function.
 
     Returns:
-        tuple: `(q, omega, path_k, Q)` where `q` is the 1D path vector for plotting;
-        `omega` is the matrix of phonon modes; `path_k` is the matrix of actual
-        k-points that were sampled (corresponding to `q`); `Q` is the list of
-        special points (in `q`) corresponding to the given `kpath`.
+        If `outfile` is None, then this method returns a dictionary that has the
+        same format as :func:`from_yaml`.
     """
-    from matdb.utility import reporoot
-    from matdb.fitting.basic import Trainer
-    from os import path, mkdir, getcwd, chdir
+    from tempfile import mkdtemp
+    from phonopy.file_IO import write_FORCE_CONSTANTS
+    from matdb.kpoints import parsed_kpath
+    from matdb.utility import execute
+    from shutil import rmtree
+    
+    #Create a temporary directory in which to work.
+    target = mkdtemp()
+    bandfile = path.join(target, "band.yaml")
+    
+    if grid is None:
+        grid = [13, 13, 13]
+    
+    #First, roll up the Hessian and write it as a FORCE_CONSTANTS file.
+    with chdir(target):
+        HR = roll(hessian)
+        write_FORCE_CONSTANTS(HR)
+        atoms.write("POSCAR", format="vasp")
+
+        #We need to create the band.conf file and write the special
+        #paths in k-space at which the phonons should be calculated.
+        atom_types = _ordered_unique(atoms.get_chemical_symbols())
+        settings = [
+            ("FORCE_CONSTANTS", "READ"),
+            ("ATOM_NAME", ' '.join(atom_types)),
+            ("DIM", ' '.join(map(str, supercell))),
+            ("MP", ' '.join(map(str, grid)))
+        ]
+
+        labels, bands = parsed_kpath(atoms)
+        bandfmt = "{0:.3f} {1:.3f} {2:.3f}"
+        sband = []        
+        for Q in bands:
+            sband.append(bandfmt.format(*Q))
+
+        settings.append(("BAND", "  ".join(sband)))
+        settings.append(("BAND_LABELS", ' '.join(labels)))
+
+        with open("band.conf", 'w') as f:
+            for k, v in settings:
+                f.write("{} = {}\n".format(k, v))
+
+    sargs = ["phonopy", "band.conf"]
+    xres = execute(sargs, target, venv=True)
+
+    if not path.isfile(bandfile): #pragma: no cover
+        msg.err("could not calculate phonon bands; see errors.")
+
+    if len(xres["error"]) > 0:
+        msg.std(''.join(xres["error"]))
+
+    result = None
+    if outfile is not None:
+        #Move the band.yaml file to the new target location.
+        from shutil import move
+        move(bandfile, outfile)
+    else:
+        result = from_yaml(bandfile)
+
+    #Remove the temporary directory that we created and return the result.
+    rmtree(target)
+    return result            
+
+def calc(atoms, cachedir=None, delta=0.01):
+    """Calculates the Hessian for a given atoms object (which *must* have an
+    attached calculator).
+
+    .. note:: We choose to use the Hessian as the fundamental quantity in
+      vibrational analysis in `matdb`.
+
+    .. note:: `atoms` will be relaxed before calculating the Hessian.
+
+    Args:
+        atoms (quippy.Atoms): atomic structure of the *supercell*.
+        cachedir (str): path to the directory where phonon calculations are
+          cached. If not specified, a temporary directory will be used.
+        supercell (tuple): number of times to duplicate the cell when
+          forming the supercell.
+        delta (float): displacement in Angstroms of each atom when computing the
+          phonons. 
+
+    Returns:
+        numpy.ndarray: Hessian matrix that has dimension `(natoms*3, natoms*3)`,
+        where `natoms` is the number of atoms in the *supercell*.
+    """
+    from ase.optimize import BFGS
+    from matdb.utility import redirect_stdout
+    from ase.vibrations import Vibrations
+        
     #The phonon calculator caches the displacements and force sets for each
     #atomic displacement using pickle. This generates three files for each
     #atomic degree of freedom (one for each cartesian direction). We want to
     #save these in a special directory.
-    if isinstance(fit, Trainer):
-        pot = fit.calculator
-        dirname = path.join(cachedir, fit.fqn)
+    if cachedir is None:
+        from tempfile import mkdtemp
+        cachedir = mkdtemp()
     else:
-        pot = fit
-        dirname = cachedir
-        
-    if not path.isdir(dirname):
-        mkdir(dirname)
-    curdir = getcwd()
+        cachedir = path.abspath(path.expanduser(cachedir))
+    if not path.isdir(cachedir):
+        mkdir(cachedir)
 
-    from ase.optimize.precon import Exp, PreconLBFGS
-    from matdb.utility import redirect_stdout
-    try:
-        chdir(dirname)
-            
-        #Relax the cell before we try and get phonons out.
-        atoms.set_calculator(pot)
-        atoms.set_cutoff(pot.cutoff())
-        minim = PreconLBFGS(atoms, precon=Exp(A=3.0))
-        minim.run(fmax=1e-7)
+    result = None
+    with chdir(cachedir):           
+        #Relax the cell before we calculate the Hessian; this gets the forces
+        #close to zero before we make harmonic approximation.
+        minim = BFGS(atoms, logfile="phonons.log")
+        minim.run(fmax=1e-5)
 
-        #Now run the phonon calculation
-        from ase.phonons import Phonons
-        if isinstance(supercell, int):
-            supercell = [supercell]*3
-        ph = Phonons(atoms, pot, supercell=tuple(supercell), delta=delta)
-
-        from sys import stdout
-        with open("phonons.log", 'w') as f:
+        vib = Vibrations(atoms, delta=delta)
+        with open("phonons.log", 'a') as f:
             with redirect_stdout(f):
-                ph.run()
+                vib.run()
 
-                #Read forces and assemble the dynamical matrix
-                ph.read(acoustic=True)
+        #Read forces and assemble the Hessian matrix.
+        vib.summary(log="vibsummary.log")
+        result = vib.H
 
-        #Conversion from eV to THz. We want to get out the phonon modes for the
-        #full path. They are in units of eV, which we want to convert to THz for
-        #comparison with the published values. We use the speed of light $c$:
-
-        #$c = f \lambda$ is the simple relation for wave velocity vs. frequency
-        #and wavelength; we combine that with the photon energy relation $E =
-        #\frac{h c}{\lambda}$ to obtain $E = h f$.
-
-        #Simplifying, we obtain: $f = \frac{E}{h}$
-        #The value for $h$ in units of `eV.s` is $h = 0.004135668 \times
-        #10^{-12}$ where the $10^{-12} gives us `eV/THz`.
-        h = 0.004135668
-        import ase.dft.kpoints as kpt
-        path_kc, q, Q = kpt.get_bandpath(kpath, atoms.cell, Npts)
-        omega = ph.band_structure(path_kc)/h #Now in units of THz
-    finally:
-        #Switch the directory back to what it was.
-        chdir(curdir)
-
-    result = {
-        "q": q,
-        "w": omega,
-        "path": path_kc,
-        "Q": Q
-    }
     return result
 
 def from_yaml(filepath):
@@ -200,24 +202,28 @@ def bandplot(phonons, names, nbands=None, style=None, figsize=(8, 6),
     Args:
         phonons (dict): keys are arbitrary ids, though they should
           correspond to `style` and `ptype`; values are `dict`
-          instances returned by either :func:`calc` or
-          :func:`from_yaml`. 
+          instances returned by :func:`from_yaml`. 
         names (list): of `str`; specifies the labels for each of the points in `Q`.
         nbands (int): number of phonon bands (dispersion curves) to
           plot. If not specified, all of them are used.
         style (dict): keys are same as for `phonons`; values are the line/scatter
           styles to use for the plotting, given as a keyword argument dictionary that
           is passed directly to the plotting method.
-        ptype (dict): keys are same as for `q` an `w`; values are one of 
+        ptype (dict): keys are same as for `phonons` an `style`; values are one of 
           ["plot", "scatter"], specifying whether to produce a line plot or scatter
           plot. If not specified, then scatter plots are used by
           default.
+        figsize (tuple): specifying the width and height (in inches) of the plot
+          that will be produced.
+        title (str): plot title override; used as-is.
+        outfile (str): if the plot should be saved, the path to the output
+          file. Output format is detected from file extension.
     """
+    import matplotlib.pyplot as plt
     if testmode:
         import matplotlib
         matplotlib.use('Agg')
-    import matplotlib.pyplot as plt
-    
+
     plt.figure(1, figsize, frameon=False)
     plt.axes([.1, .07, .67, .85])
 
@@ -234,9 +240,7 @@ def bandplot(phonons, names, nbands=None, style=None, figsize=(8, 6),
     
     for label, pdict in phonons.items():
         omega = pdict["w"]
-        qs = pdict["q"].copy()
-        scaling = (maxq-minq)/(np.max(qs)-np.min(qs))
-        qs = scaling*(qs-np.min(qs))
+        qs = pdict["q"]
         
         if len(omega.shape) == 1:
             omega.shape = (len(omega), 1)
