b2cc040df504edfd05f7d9c5ec1d360fdcf0fca3
Brayden Bekker <brayden.bekker@byu.edu>
2018-05-16 12:22:57 -0600

    Update to group config index.

--- matdb/database/distortion.py
+++ matdb/database/distortion.py
@@ -13,7 +13,7 @@ class Distortion(Group):
     the atom positions or displaces the atoms randomly within a normal
     distribution of some standard deviation, std.
     Args:
-        name (str): Distortion: 'Dist'
+        name (str): default name Distortion
         rattle (float): the standard deviation of the normal distribution
             of atom deviations.
         ran_seed (hashable):(=1 default) seed for the normal distribution
@@ -104,14 +104,13 @@ class Distortion(Group):
         group.
         """
         if len(self.sequence) == 0:
-            return len(self.atoms_paths) == self.nconfigs
+            return len(self.atoms_paths()) == self.nconfigs
         else:
             result = []
             for g in self.sequence.values():
                 result.extend(g.fitting_configs)
             return result
 
-    @property
     def rset(self):
         """Returns a :class:`matdb.atoms.AtomsList`, one for each config in the
         latest result set.
@@ -120,8 +119,8 @@ class Distortion(Group):
             # Return the configurations from this group; it is at the
             # bottom of the stack
             result = AtomsList()
-            for epath in self.fitting_configs:
-                result.append(Atoms(path.join(epath, 'atoms.h5')))
+            for epath in self.atoms_paths():
+                result.append(Atoms(path.join(epath, 'pre_comp_atoms.h5')))
             return result
         else:
             result = []
@@ -135,7 +134,7 @@ class Distortion(Group):
         """
         self._expand_sequence()
         if len(self.sequence) == 0:
-            result = len(self.atoms_paths) == self.nconfigs
+            result = len(self.atoms_paths()) == self.nconfigs
             if not result:
                 msg.std("{} is not ready. Exiting.".format(self.root), 2)
             return result
@@ -149,24 +148,23 @@ class Distortion(Group):
                     ready = True
             return ready
 
-    @property
     def atoms_paths(self):
-        """Returns a list of full paths to the folders that have `atoms.json` objects
+        """Returns a list of full paths to the folders that have `atoms.h5` objects
         for the latest result set.
         """
         result = []
         for duid in self.duids:
             folder = self.index[duid]
-            target = path.join(folder, "atoms.h5")
+            target = path.join(folder, "pre_comp_atoms.h5")
             if path.isfile(target):
                 result.append(folder)
         return result
 
-    def setup(self, rerun=False):
+    def setup(self, rerun=0):
         """Sets up a copy of the atoms object for the current config.
         Args:
-            rerun (bool): when True, recreate the folders even if they
-             already exist.
+            rerun (int): when > 0, recreate job files; if > 1, recreate the
+              folders even if they already exist.
         """
         super(Distortion, self).setup(self._setup_configs, rerun)
         if len(self.sequence) != 0:
@@ -178,7 +176,7 @@ class Distortion(Group):
         self.save_index()
         self.save_pkl(self.duids, self.duid_file)
 
-    def _setup_configs(self, rerun=False):
+    def _setup_configs(self, rerun=0):
         """Loops over the distortion routine until the desired number of
         configurations have been reached
         Attributes:
@@ -186,6 +184,8 @@ class Distortion(Group):
         Args:
             group (:class:`matdb.database.basic.Group`): An instance of the
                  group class.
+            rerun (int): when > 0, recreate job files; if > 1, recreate the
+              folders even if they already exist.
         """
         from hashlib import sha1
         dists = self._get_distortion()
@@ -194,7 +194,7 @@ class Distortion(Group):
             self.duids = []
         for dist in dists:
             self.create(dist)
-            chem_form = dist.get_chemical_formula(mode='reduce')
+            chem_form = str(dist.get_cell()[0])
             duid = sha1(chem_form).hexdigest()
             self.duids.append(duid)
             self.index[duid] = self.configs[len(self.configs)]
