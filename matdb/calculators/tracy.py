06f42fd800c92a3f9fd5dfecb4cf773e17d5fe5f
Wiley Morgan <wiley.s.morgan@gmail.com>
2018-05-29 13:48:07 -0600

    Fixed minor bugs in tracy calculator.

--- matdb/calculators/tracy.py
+++ matdb/calculators/tracy.py
@@ -4,6 +4,8 @@ those calculations that will be added to the Tracy compute queue.
 from datetime import datetime
 from os import path
 from random import seed, uniform
+import json
+import abc
 
 from matdb.database.utility import make_primitive
 from matdb.descriptors import soap
@@ -23,14 +25,14 @@ class Tracy(AsyncCalculator):
     def __init__(self, folder, role=None, notifications=None,
                  group_preds=None, contract_preds=None, ecommerce=None,
                  contract_priority=None, max_time=None, min_flops=None,
-                 min_mem=None, ncores=None, max_net_lat=None):
+                 min_mem=None, ncores=None, max_net_lat=None, min_ram=None):
 
         # If this folder has already been submitted then there will be
         # a file containing the contract number in it. We want to read
         # this in so we can check it's status later.
         self.folder = folder
         if path.isfile(path.join(folder, "contract.txt")):
-            with open(path.join(folder, "contract.txt"). "r") as f:        
+            with open(path.join(folder, "contract.txt"), "r") as f:        
                 self.contract_id = f.readline().strip()
         else:
             self.contract_id = None
@@ -48,6 +50,8 @@ class Tracy(AsyncCalculator):
         self.group_preds = group_preds
         self.contract_preds = contract_preds
         self.contract_priority = contract_priority if contract_priority is not None else 1
+        self.sys_specs = {"max_time": max_time, "min_flops": min_flops, "min_mem": min_mem,
+                          "ncores": ncores, "min_ram": min_ram, "max_net_lat": max_net_lat}
 
     def _compress_struct(self, atoms):
         """Compresess the input atoms object so that it is ready to be sent to
@@ -64,7 +68,7 @@ class Tracy(AsyncCalculator):
 
         result = {"a": [list(a) for a in a_vecs],
                   "b": [list(b) for b in pos],
-                  "t": [self.type_map[i] for i in types]
+                  "t": [self.type_map[i] for i in types],
                   "h": [hnf[0][0], hnf[1][0], hnf[1][1], hnf[2][0], hnf[2][1], hnf[2][2]]}
         return result
 
@@ -94,17 +98,24 @@ class Tracy(AsyncCalculator):
         """Submits the job to the authetication code before being sent to the queue.
         """
         self.get_input_dict()
-        self.package = {}
-        self.package["MatDB ID"] = self.atoms.uuid
-        self.package["MatDB Group ID"] = self.atoms.group_uuid
+        package = {}
+        package["MatDB ID"] = self.atoms.uuid
+        package["MatDB Group ID"] = self.atoms.group_uuid
         # package["Source ID"] = Get this from Josh's scripit
-        self.package["Source"] = def._get_source()
-        self.package["Contract Type"] = self.contract_type
-        self.package["Input Dictionary"] = self.input_dict
-        self.package["Input Dictionary"]["cryst"] = self._compress_struct(self.atoms)
-        self.package["Before fingerprint"] = soap(self.atoms)
-        self.package["Contract Priority"] = self.contract_priority
-        self.package["eCommerce Priority"] = self.ecommerce
+        package["Source"] = self._get_source()
+        package["Contract Type"] = self.contract_type
+        package["Input Dictionary"] = self.input_dict
+        package["Input Dictionary"]["cryst"] = self._compress_struct(self.atoms)
+        package["Before fingerprint"] = soap(self.atoms)
+        package["Contract Priority"] = self.contract_priority
+        package["eCommerce Priority"] = self.ecommerce
+        package["Maximum Processing Time"] = self.sys_specs["max_time"]
+        package["Minimum FLOPS"] = self.sys_specs["min_flops"]
+        package["Minimum RAM"] = self.sys_specs["min_ram"]
+        package["Minimum Storage"] = self.sys_specs["min_mem"]
+        package["Number of Cores"] = self.sys_specs["ncores"]
+        package["Maximum Network Latency"] = self.sys_specs["max_net_lat"]
+        
         if self.can_execute(self, self.folder):
             package["Date Ready"] = datetime.now()
         job_reqs = self._get_job_reqs()
@@ -117,8 +128,10 @@ class Tracy(AsyncCalculator):
             package["Contract Predecossors"] = self.contract_preds
 
         target = path.join(folder, "submission.json")
+
+        with open(target, "w+") as f:
+            json.dump(package, f)
         
-    
 
     def can_extract(self, folder):
         """Returns `True` if the calculation has been completed and the data
@@ -176,8 +189,8 @@ class Tracy_QE(Tracy, Qe):
         if self.ran_seed is not None:
             seed(self.ran_seed)            
 
-        Qe.__init__(self, atoms, folder, cantr_dir, ran_seed, **QE_input)
-        Tracy.__init__(self, **tracy_input)
+        Qe.__init__(self, atoms, folder, contr_dir, ran_seed, **QE_input)
+        Tracy.__init__(self, folder, **tracy_input)
 
     def _check_potcar(self):
         """We don't construct the potetial files on the user's end so we don't
@@ -224,7 +237,7 @@ class Tracy_QE(Tracy, Qe):
                         self.input_dict[key][sub_key] = val
                     elif line.strip() == "/":
                         continue
-                    elif k_val is not and key == "k_points":
+                    elif k_val is not None and key == "k_points":
                         self.input_dict[key][k_val] = line.strip()
                     elif key == "atomic_species":
                         species = line.strip().split()[0]
@@ -238,7 +251,17 @@ class Tracy_QE(Tracy, Qe):
                     
                 if key not in input_dict.keys():
                     self.input_dict[key] = {}
-                        
+
+    def can_execute(self, folder):
+        """Returns True if the specified file is ready to be submitted to the queue.
+        
+        Args:
+            folder (str): the path to the folder.
+        """
+        qe_ready = Qe.can_execute(folder)
+        sub_ready = path.isfile(path.join(folder,"submission.json"))
+        return qe_ready and sub_ready
+                    
     def extract(self, folder):
         """Extracts the results from the returned data package.
         """
