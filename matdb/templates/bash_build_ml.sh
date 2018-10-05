255f7dee49df87a87a99119670e4a6efa94e42f2
Kevin Li <kevin.li@norimaconsulting.com>
2018-10-05 09:03:52 -0500

    Fixed the problem that it runs only for the first sub-directory for template bash_build_ml.sh.

--- matdb/templates/bash_build_ml.sh
+++ matdb/templates/bash_build_ml.sh
@@ -4,7 +4,7 @@
 workdir=$(echo {{execution_path}} | awk -F'/[^/]*$' '{print $1}' )
 
 for f in *; do
-    if [ -d ${f} ]; then
+    if [ -d ${workdir}/${f} ]; then
         cd ${workdir}/${f}
         {{ exec_path }} -i espresso.pwi > espresso.out
     fi
