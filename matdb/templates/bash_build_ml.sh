2d23229557ac9ac2c9118304d8464378d559bc6e
Kevin Li <kevin.li@norimaconsulting.com>
2019-01-29 11:38:39 -0600

    temp fix to ingore the configurations which has already been calculated by DFT_QE

--- matdb/templates/bash_build_ml.sh
+++ matdb/templates/bash_build_ml.sh
@@ -5,6 +5,12 @@ workdir=$(echo {{execution_path}} | awk -F'/[^/]*$' '{print $1}' )
 
 for f in *; do
     if [ -d ${workdir}/${f} ]; then
+        # ignore the configuration which has already been calculated
+        # this is temp fix for DFT_QE
+        if [ -f ${workdir}/${f}/espresso.out ]; then
+           continue
+        fi
+
         cd ${workdir}/${f}
         {{ exec_path }} -i espresso.pwi > espresso.out
     fi
