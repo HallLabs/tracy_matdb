ef3b1c23005c725b7366d2630cb439b181c0f3c0
Kevin Li <kevin.li@norimaconsulting.com>
2019-02-26 11:41:38 -0600

    Added timeout to the QE calculation process.

--- matdb/templates/bash_build_ml.sh
+++ matdb/templates/bash_build_ml.sh
@@ -6,12 +6,14 @@ workdir=$(echo {{execution_path}} | awk -F'/[^/]*$' '{print $1}' )
 for f in *; do
     if [ -d ${workdir}/${f} ]; then
         # ignore the configuration which has already been calculated
-        # this is temp fix for DFT_QE
+        # this is a temp fix for DFT_QE
         if [ -f ${workdir}/${f}/espresso.out ]; then
            continue
         fi
 
         cd ${workdir}/${f}
-        {{ exec_path }} -i espresso.pwi > espresso.out
+        # QE calculation will be timed out if runs up to the time out parameter in the yml file
+        # Note that to turn off timeout, specify 0 for the exec_time_out_minutes parameter
+        timeout -sTERM {{ exec_time_out_minutes }}m {{ exec_path }} -i espresso.pwi > espresso.out
     fi
 done
