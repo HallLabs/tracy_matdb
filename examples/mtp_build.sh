b52361f03ad67742662f2c956bfadbe6d8f3c16a
Kevin Li <kevin.li@norimaconsulting.com>
2019-03-06 11:45:06 -0600

    Refactored the startup scripts. Added function to check next_cell_threshold to decide if needs to move on the next cell.

--- examples/mtp_build.sh
+++ examples/mtp_build.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-matdb_build.py Qe_CoWV -s --verbose 5
+# grab the YAML specification file from the command line argument if it's specified
+if [ $# -gt 0 ]; then
+  SPEC_FILE=$1
+fi
+
+# grab the YAML specification file from the current directory if it's not specified from the command line
+if [ ! -f "$SPEC_FILE" ]; then
+  SPEC_FILE=$(ls *.yml 2>/dev/null)
+fi
+
+# exit if a YAML specification file could not be found
+if [ ! -f "$SPEC_FILE" ]; then
+  echo "No YAML specification file."
+  exit 1
+fi
+
+# grab the base name of the YAML specification file
+SPEC_FILE_BASE=$(echo "$SPEC_FILE" | cut -f 1 -d '.')
+
+matdb_build.py ${SPEC_FILE_BASE} -s --verbose 5
 if [ $? -ne 0 ]; then
   echo "mtp_build step 1 Error"
   exit 1
 fi
 
-matdb_build.py Qe_CoWV -x --verbose 5
+matdb_build.py ${SPEC_FILE_BASE} -x --verbose 5
 if [ $? -ne 0 ]; then
   echo "mtp_build step 2 Error"
   exit 1
 fi
 
-matdb_build.py Qe_CoWV -e --verbose 5
+matdb_build.py ${SPEC_FILE_BASE} -e --verbose 5
 if [ $? -ne 0 ]; then
   echo "mtp_build step 3 Error"
   exit 1
--- examples/mtp_train_first_iter.sh
+++ examples/mtp_train_first_iter.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
+# grab the YAML specification file from the command line argument if it's specified
+if [ $# -gt 0 ]; then
+  SPEC_FILE=$1
+fi
+
+# grab the YAML specification file from the current directory if it's not specified from the command line
+if [ ! -f "$SPEC_FILE" ]; then
+  SPEC_FILE=$(ls *.yml 2>/dev/null)
+fi
+
+# exit if a YAML specification file could not be found
+if [ ! -f "$SPEC_FILE" ]; then
+  echo "No YAML specification file."
+  exit 1
+fi
+
+# grab the base name of the YAML specification file
+SPEC_FILE_BASE=$(echo "$SPEC_FILE" | cut -f 1 -d '.')
+
 COUNTER=1
 STEP=1
 echo "Step $STEP starting at: `date --iso-8601=seconds`"
-matdb_train.py Qe_CoWV -t --verbose 5
+matdb_train.py ${SPEC_FILE_BASE} -t --verbose 5
 if [ $? -ne 0 ]; then
   echo "mtp_train_first_iteration step $STEP Errored"
   exit 1
@@ -12,7 +31,7 @@ fi
 while [ $COUNTER -lt 5 ]; do
   let STEP+=1
   echo "Step $STEP starting at: `date --iso-8601=seconds`"
-  matdb_train.py Qe_CoWV -x --verbose 5
+  matdb_train.py ${SPEC_FILE_BASE} -x --verbose 5
   if [ $? -ne 0 ]; then
     echo "mtp_train_first_iteration step $STEP Errored"
     exit 1
@@ -20,7 +39,7 @@ while [ $COUNTER -lt 5 ]; do
 
   let STEP+=1
   echo "Step $STEP starting at: `date --iso-8601=seconds`"
-  matdb_train.py Qe_CoWV -t --verbose 5
+  matdb_train.py ${SPEC_FILE_BASE} -t --verbose 5
   if [ $? -ne 0 ]; then
     echo "mtp_train_first_iteration step $STEP Errored"
     exit 1
