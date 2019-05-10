19f253cc0059cef0c7a56e65ec0646093c4f65b8
Ming Zhang <ming.zhang@norimaconsulting.com>
2019-05-10 11:18:39 -0400

    remove .pem file and updated the deploy script.

--- tracy_deploy/deploy_aws.sh
+++ tracy_deploy/deploy_aws.sh
@@ -1,8 +1,15 @@
-scp -i matdb-test.pem matdb_stable.jar ubuntu@ec2-18-220-253-111.us-east-2.compute.amazonaws.com:~/
-scp -i matdb-test.pem mlip4tracy_stable.jar ubuntu@ec2-18-220-253-111.us-east-2.compute.amazonaws.com:~/
-scp -i matdb-test.pem tracy_science.jar ubuntu@ec2-18-220-253-111.us-east-2.compute.amazonaws.com:~/
-scp -i matdb-test.pem pslibrary4tracy.jar ubuntu@ec2-18-220-253-111.us-east-2.compute.amazonaws.com:~/
-scp -i matdb-test.pem dft_qe4tracy.jar ubuntu@ec2-18-220-253-111.us-east-2.compute.amazonaws.com:~/
-scp -i matdb-test.pem ubuntu4tracy.jar ubuntu@ec2-18-220-253-111.us-east-2.compute.amazonaws.com:~/
-scp -i matdb-test.pem ubuntu.jar ubuntu@ec2-18-220-253-111.us-east-2.compute.amazonaws.com:~/
-scp -i matdb-test.pem load_docker_images_aws.sh ubuntu@ec2-18-220-253-111.us-east-2.compute.amazonaws.com:~/
+IDENTITY_FILE="matdb-test.pem" 
+if [ -f "$IDENTITY_FILE" ]; then
+    echo "start copying to EC2 for all docker image jar files...."
+else 
+    echo ".pem file does not exist"
+    exit 1
+fi
+scp -i $IDENTITY_FILE  matdb_stable.jar ubuntu@ec2-18-220-253-111.us-east-2.compute.amazonaws.com:~/
+scp -i $IDENTITY_FILE  mlip4tracy_stable.jar ubuntu@ec2-18-220-253-111.us-east-2.compute.amazonaws.com:~/
+scp -i $IDENTITY_FILE  tracy_science.jar ubuntu@ec2-18-220-253-111.us-east-2.compute.amazonaws.com:~/
+scp -i $IDENTITY_FILE  pslibrary4tracy.jar ubuntu@ec2-18-220-253-111.us-east-2.compute.amazonaws.com:~/
+scp -i $IDENTITY_FILE  dft_qe4tracy.jar ubuntu@ec2-18-220-253-111.us-east-2.compute.amazonaws.com:~/
+scp -i $IDENTITY_FILE  ubuntu4tracy.jar ubuntu@ec2-18-220-253-111.us-east-2.compute.amazonaws.com:~/
+scp -i $IDENTITY_FILE  ubuntu.jar ubuntu@ec2-18-220-253-111.us-east-2.compute.amazonaws.com:~/
+scp -i $IDENTITY_FILE load_docker_images_aws.sh ubuntu@ec2-18-220-253-111.us-east-2.compute.amazonaws.com:~/
